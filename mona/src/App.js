a244bce7fe40f902cf526475039697869131860d
tman0004 <tman0004@gmail.com>
2021-05-01 17:59:41 -0700

    working map

--- mona/src/App.js
+++ mona/src/App.js
@@ -3,30 +3,23 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-}));
-
-const App = () => {
-  const classes = useStyles();
-
+function App() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <Typography variant="h6" color="inherit">
-            Welcome, developer | My React and Leaflet app
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  )
+}
 
 export default App;

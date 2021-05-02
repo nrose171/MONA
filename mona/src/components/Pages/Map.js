20b888c60cbd55d596a3519fb1d424016cbd74da
nrose171 <nrose171@gmail.com>
2021-05-01 20:29:57 -0700

    Added map image and marker popup image

--- mona/src/components/Pages/Map.js
+++ mona/src/components/Pages/Map.js
@@ -15,16 +15,17 @@ function Map() {
             <h1>Map</h1>
             <MapContainer center={[-34.43537, 150.45502]} zoom={10} scrollWheelZoom={true}>
 
-                <ImageOverlay
-                    url="mona_map.png"
-                    bounds={[[-34.43537, 150.45502], [-34.67063, 150.21057]]}
-                />
+              <ImageOverlay
+                url="https://i.imgur.com/Lha3Pz4.png"
+                bounds={[[-34.43537, 150.45502], [-34.67063, 150.21057]]}
+              />
 
-                <Marker position={[-34.43537, 150.45502]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+              <Marker position={[-34.43537, 150.45502]}>
+                <Popup position={[-34.43537, 150.45502]}>
+                  <center>Mona Lisa</center>
+                  <img src="https://i.imgur.com/lmJy0b5.jpg" height="100px" width="100px"/>
+                </Popup>
+              </Marker>
 
             </MapContainer>
         </div>

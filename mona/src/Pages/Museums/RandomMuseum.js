e90422011034cce4503caa697b2afafcd48bf86f
tman0004 <tman0004@gmail.com>
2021-05-02 21:36:57 -0700

    Added another dummy museum. Moved museum pages into own folder
    Also changed the value paths for all routing for simplicity.

---
+++ mona/src/Pages/Museums/RandomMuseum.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RandomMuseum() {
+  return (
+      <div>
+          <h1 style={{marginTop: '0vh'}}> Just Another Museum</h1>
+      </div>
+  );
+}
+
+export default RandomMuseum;

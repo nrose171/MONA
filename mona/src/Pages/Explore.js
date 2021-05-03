337ab99f5e76b72f8661b2dfdb943ded2314f909
tman0004 <tman0004@gmail.com>
2021-05-02 21:27:52 -0700

    Removed nav.js and formatted dropdown

--- mona/src/Pages/Explore.js
+++ mona/src/Pages/Explore.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import {BrowserRouter as Router, Switch, Route, BrowserRouter} from 'react-router-dom';
 import Louvre from "./Louvre"
-import { makeStyles, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { makeStyles, FormControl, InputLabel, Select, MenuItem, Box } from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,27 +24,26 @@ function Explore() {
       <div>
         <h1 style={{marginTop: '10vh'}}> Explore</h1>
         <Router>
-          <div>
-            <FormControl className={classes.formControl}>
-            <InputLabel>Museums</InputLabel>
-            <Select
-              value={value}
-              onChange={handleChange}
-            >
-              <MenuItem 
-                value={"/Pages/Louvre"}
-                component={Link}
-                to={"/Pages/Louvre"}
+            <Box position={"absolute"} left={'45%'}>
+              <FormControl className={classes.formControl}>
+              <InputLabel>Museums</InputLabel>
+              <Select
+                value={value}
+                onChange={handleChange}
               >
-                Louvre Museum
-              </MenuItem>
-            </Select>
-          </FormControl>
-            <hr />
+                <MenuItem 
+                  value={"/Pages/Louvre"}
+                  component={Link}
+                  to={"/Pages/Louvre"}
+                >
+                  Louvre Museum
+                </MenuItem>
+              </Select>
+              </FormControl>
+            </Box>
             <Switch>
               <Route path="/Pages/Louvre" exact component={Louvre}/>
             </Switch>
-          </div>
         </Router>
       </div>
   );

1a0f8a593979e3bd1743be85107b7eb3e242182c
tman0004 <tman0004@gmail.com>
2021-05-02 20:04:09 -0700

    working

--- mona/src/Pages/Explore.js
+++ mona/src/Pages/Explore.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, BrowserRouter} from 'react-router-dom';
+import Louvre from "./Louvre"
 
 function Explore() {
   return (
       <div>
           <h1 style={{marginTop: '10vh'}}> Explore</h1>
+          <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/Pages/Louvre">Home</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route path="/Pages/Louvre" exact component={Louvre}/>
+        </Switch>
+      </div>
+    </Router>
       </div>
   );
 }
---
+++ mona/src/Pages/Louvre.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Louvre() {
+  return (
+      <div>
+          <h1 style={{marginTop: '10vh'}}> dasfasdfasdf</h1>
+      </div>
+  );
+}
+
+export default Louvre;

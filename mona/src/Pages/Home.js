16512a3ea4b007be7fcf598082c6e5bf8443440b
tman0004 <tman0004@gmail.com>
2021-05-03 16:47:52 -0700

    Added shadow to cards

--- mona/src/Pages/Home.js
+++ mona/src/Pages/Home.js
@@ -1,19 +1,6 @@
 import React, { useState, useRef } from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import './Pages.css';
+import { Grid, Box, Typography, IconButton, Collapse, CardActions, CardContent, CardMedia, CardHeader, Card, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -29,11 +16,11 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     border: '2px solid', 
-    borderColor: "#668cff"
+    borderColor: "#668cff",
   },
   cardMedia: {
     width: 500,
-    height: 300
+    height: 250
   },
   cardContent: {
     flexGrow: 1,
@@ -62,43 +49,47 @@ function Home() {
         <Container className={classes.cardGrid}>
           <Grid container spacing={4}>
             <Grid item xs={5}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://i.imgur.com/1NTtRmM.jpg"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    The Louvre
-                  </Typography>
-                  <Typography>
-                    The Louvre, or the Louvre Museum, is the world's largest art museum and a historic monument in Paris, France, 
-                    and is best known for being the home of the Mona Lisa. 
-                    A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement.
-                  </Typography>
-                </CardContent>
-              </Card>
+              <Box boxShadow={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://i.imgur.com/1NTtRmM.jpg"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      The Louvre
+                    </Typography>
+                    <Typography>
+                      The Louvre, or the Louvre Museum, is the world's largest art museum and a historic monument in Paris, France, 
+                      and is best known for being the home of the Mona Lisa. 
+                      A central landmark of the city, it is located on the Right Bank of the Seine in the city's 1st arrondissement.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Box>
             </Grid>
             
             <Grid item xs={2}/>
 
             <Grid item xs={5}>
-              <Card className={classes.card}>
-                <CardMedia
-                  className={classes.cardMedia}
-                  image="https://i.imgur.com/N0QWfnd.jpg"
-                />
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                  The National Museum of Natural History
-                  </Typography>
-                  <Typography>
-                    The National Museum of Natural History is a natural history museum administered by the Smithsonian Institution, 
-                    located on the National Mall in Washington, D.C., United States. 
-                    The museum's collections contain over 145 million specimens and artifacts, the largest natural history collection in the world.
-                  </Typography>
-                </CardContent>
-              </Card>
+              <Box boxShadow={3}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://i.imgur.com/N0QWfnd.jpg"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    The National Museum of Natural History
+                    </Typography>
+                    <Typography>
+                      The National Museum of Natural History is a natural history museum administered by the Smithsonian Institution, 
+                      located on the National Mall in Washington, D.C., United States. 
+                      The museum's collections contain over 145 million specimens and artifacts, the largest natural history collection in the world.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Box>
             </Grid>
           </Grid>   
           

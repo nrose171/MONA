13ca4c991bd3b9134d6ffbd9c1e016631201ed1d
tman0004 <tman0004@gmail.com>
2021-05-03 20:37:34 -0700

    navbar formatted better

--- mona/src/App.js
+++ mona/src/App.js
@@ -36,7 +36,7 @@ function App() {
                     </Box>
                   </Grid>
                   <Grid item xs={6} />
-                  <Grid item xs={3}>
+                  <Grid item xs={4}>
                     <Box pt={2}>
                       <Tabs
                         value={value}
